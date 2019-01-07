7b2292632244a029fceda408457de5e413a2c073
Vidit Shah <vidit0210@gmail.com>
2019-01-07 14:04:39 +0530

    faker

--- components/src/index.js
+++ components/src/index.js
@@ -1,7 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6pm</span>
+          </div>
+          <div className="text">Nice blog Post!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 ReactDOM.render(<App />, document.querySelector("#root"));
