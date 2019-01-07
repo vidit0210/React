8e49a771a10d83e0275015f21015ec53182ead39
Vidit Shah <vidit0210@gmail.com>
2019-01-07 11:49:15 +0530

    html-jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,7 +4,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter Name
+      </label>
+      <input type="text" id="name" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        Submit
+      </button>
+    </div>
+  );
 };
+
 //Take that React Component and place it on Browser
 ReactDOM.render(<App />, document.querySelector("#root"));
