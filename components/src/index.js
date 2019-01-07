cdf506dd35a694c5e36b3a481872705b5a347109
Vidit Shah <vidit0210@gmail.com>
2019-01-07 18:12:09 +0530

    props

--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CD from "./CommentDetail";
+import fake from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CD />
-      <CD />
-      <CD />
-      <CD />
+      <CD
+        author="Vidit"
+        timeAgo="Today at 2am"
+        content="Well explained"
+        avatar={fake.image.avatar()}
+      />
+      <CD
+        author="Rahul"
+        timeAgo="Today at 2am"
+        content="I agree!"
+        avatar={fake.image.avatar()}
+      />
     </div>
   );
 };
